9cd8641e4d04195970752772fa74422429d8bb65
ayush <ayushsharma12345qwerty@gmail.com>
2025-10-27 23:47:32 +0530

    queue basic op

--- LearnQueues/LearnQueue.java
+++ LearnQueues/LearnQueue.java
@@ -7,5 +7,19 @@ public class LearnQueue {
     public static void main(String[] args) {
         System.out.println("This is a simple LearnQueue class.");
         Queue<Integer> q= new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        System.out.println(q);
+        System.out.println(q.poll());
+        System.out.println(q);
+        System.out.println(q.peek());
+        System.out.println(q);
+
+        while(!q.isEmpty()){
+            System.out.println(q.poll());
+        }
+
+        System.out.println(q.poll());
     }
 }
