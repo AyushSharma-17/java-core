b3daef60b486c8847e69e3d3806c9cc3ef68d159
ayush <ayushsharma12345qwerty@gmail.com>
2025-10-27 23:43:09 +0530

    queue1

---
+++ LearnQueues/LearnQueue.java
@@ -0,0 +1,11 @@
+package LearnQueues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LearnQueue {
+    public static void main(String[] args) {
+        System.out.println("This is a simple LearnQueue class.");
+        Queue<Integer> q= new LinkedList<>();
+    }
+}
