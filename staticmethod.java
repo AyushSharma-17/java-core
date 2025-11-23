994d9e4ecddeed04c98d15e3e224d2e168f76ab7
ayush <ayushsharma12345qwerty@gmail.com>
2025-11-24 00:09:19 +0530

    static method

---
+++ staticmethod.java
@@ -0,0 +1,26 @@
+public class staticmethod {
+    public static void main(String[] args) {
+        device1 d1 = new device1();
+        d1.brand = "apple";
+        d1.name = "iphone";
+        d1.modelno = 12;
+        d1.show();
+        // call by class name only no object calling
+        device1.show2();
+
+    }
+}
+
+class device1 {
+    String brand;
+    String name;
+    int modelno;
+
+    void show() {
+        System.out.println("simple show");
+    }
+
+    static void show2() {
+        System.out.println("static show2");
+    }
+}
