f4af9173d8405e6d7e7cd2534245d77b06bd6e7c
ayush <ayushsharma12345qwerty@gmail.com>
2026-03-25 00:09:59 +0530

    arraylist basic operation

---
+++ arraylst.java
@@ -0,0 +1,14 @@
+
+import java.lang.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class arraylst {
+    public static void main(String[] args) {
+        Collection<Integer> list1= new ArrayList<>();
+        list1.add(10);
+        list1.add(20);
+        list1.add(30);      
+        System.out.println(list1);
+    }
+}
